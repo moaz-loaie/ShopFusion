58e9e5778c93bed40529ef615c1c7ea39c642266
moaz-loaie <mo3azloaie@gmail.com>
2025-07-31 20:01:31 +0300

    chore(server): update config and housekeeping files
    - Updated .sequelizerc, jest, eslint, prettier, and package configs
    - Synced package.json and lockfile for dependency consistency
    - Prepares server for modern development and testing workflows

--- server/jest.config.js
+++ server/jest.config.js
@@ -6,11 +6,10 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8", // or 'babel'
-  // A list of paths to directories that Jest should use to search for files in
+  coverageProvider: "v8", // or 'babel'  // A list of paths to directories that Jest should use to search for files in
   roots: ["<rootDir>/tests"],
   // Setup files to run before test framework installation
-  // setupFilesAfterEnv: ['<rootDir>/tests/setup.js'], // If you need setup
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   // Test timeout
   testTimeout: 30000, // Increase timeout for integration tests involving DB calls
 };
